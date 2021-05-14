086630af842a2edbdff5d7fe6e8c9dbfa3e790b0
imthegrv <imthegrv@gmail.com>
2021-05-15 01:18:18 +0530

    n

---
+++ .nuxt/components/readme 2.md
@@ -0,0 +1,23 @@
+# Discovered Components
+
+This is an auto-generated list of components discovered by [nuxt/components](https://github.com/nuxt/components).
+
+You can directly use them in pages and other components without the need to import them.
+
+**Tip:** If a component is conditionally rendered with `v-if` and is big, it is better to use `Lazy` or `lazy-` prefix to lazy load.
+
+- `<HomeSlider>` | `<home-slider>` (components/holidays/HomeSlider.vue)
+- `<Blogcards>` | `<blogcards>` (components/blog/Blogcards.vue)
+- `<SideNav>` | `<side-nav>` (components/account/SideNav.vue)
+- `<Footer>` | `<footer>` (components/Footer.vue)
+- `<LangSwitcher>` | `<lang-switcher>` (components/LangSwitcher.vue)
+- `<Nav>` | `<nav>` (components/Nav.vue)
+- `<Navdark>` | `<navdark>` (components/Navdark.vue)
+- `<Offercardcarousel>` | `<offercardcarousel>` (components/Offercardcarousel.vue)
+- `<Offerscardtab>` | `<offerscardtab>` (components/Offerscardtab.vue)
+- `<Popularoutes>` | `<popularoutes>` (components/Popularoutes.vue)
+- `<Promotionsbanner>` | `<promotionsbanner>` (components/Promotionsbanner.vue)
+- `<Sliderhomepage>` | `<sliderhomepage>` (components/Sliderhomepage.vue)
+- `<Tourcardcarousel>` | `<tourcardcarousel>` (components/Tourcardcarousel.vue)
+- `<UserAuthForm>` | `<user-auth-form>` (components/UserAuthForm.vue)
+- `<Langlist>` | `<langlist>` (components/langlist.vue)
---
+++ .nuxt/nuxt-i18n/options 2.js
@@ -0,0 +1,48 @@
+import locale7744c453 from '../../locales/es.js'
+import locale77427e78 from '../../locales/en.js'
+
+export const Constants = {
+  COMPONENT_OPTIONS_KEY: "nuxtI18n",
+  STRATEGIES: {"PREFIX":"prefix","PREFIX_EXCEPT_DEFAULT":"prefix_except_default","PREFIX_AND_DEFAULT":"prefix_and_default","NO_PREFIX":"no_prefix"},
+}
+export const nuxtOptions = {
+  isUniversalMode: true,
+  trailingSlash: undefined,
+}
+export const options = {
+  vueI18n: {},
+  vueI18nLoader: false,
+  locales: [{"name":"Espaneol","code":"es","iso":"es","file":"es.js","icon":"https://www.happyvoyaging.com/themes/mmpt21/assets/img/flag_codes/SP.png"},{"name":"English","code":"en","iso":"en-IN","file":"en.js","icon":"https://www.happyvoyaging.com/themes/mmpt21/assets/img/flag_codes/US.png"}],
+  defaultLocale: "en",
+  defaultDirection: "ltr",
+  routesNameSeparator: "___",
+  defaultLocaleRouteNameSuffix: "default",
+  strategy: "prefix",
+  lazy: false,
+  langDir: "/Users/vin/Documents/Website Projects/hvapp/locales",
+  rootRedirect: null,
+  detectBrowserLanguage: {"alwaysRedirect":true,"cookieCrossOrigin":false,"cookieDomain":null,"cookieKey":"lang","cookieSecure":false,"fallbackLocale":"en","onlyOnNoPrefix":false,"onlyOnRoot":false,"useCookie":true},
+  differentDomains: false,
+  seo: false,
+  baseUrl: "",
+  vuex: {"moduleName":"i18n","syncLocale":false,"syncMessages":false,"syncRouteParams":true,"mutations":{"setLocale":"I18N_SET_LOCALE","setMessages":false},"preserveState":false},
+  parsePages: true,
+  pages: {},
+  skipSettingLocaleOnNavigate: false,
+  beforeLanguageSwitch: () => null,
+  onBeforeLanguageSwitch: () => {},
+  onLanguageSwitched: (previous, current) => {
+      if (process.client) {
+        const DATE = new Date();
+        DATE.setTime(DATE.getTime() + 365 * 24 * 3600 * 1000);
+        document.cookie = 'lang=' + current + '; path=/; expires=' + DATE.toUTCString();
+      }
+    },
+  normalizedLocales: [{"name":"Espaneol","code":"es","iso":"es","file":"es.js","icon":"https://www.happyvoyaging.com/themes/mmpt21/assets/img/flag_codes/SP.png"},{"name":"English","code":"en","iso":"en-IN","file":"en.js","icon":"https://www.happyvoyaging.com/themes/mmpt21/assets/img/flag_codes/US.png"}],
+  localeCodes: ["es","en"],
+}
+
+export const localeMessages = {
+  'es.js': () => Promise.resolve(locale7744c453),
+  'en.js': () => Promise.resolve(locale77427e78),
+}
