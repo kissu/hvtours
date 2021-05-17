7060b33295f0b314baeb448ee1e14db632d51ece
Konstantin BIFERT <konstantin.bifert@gmail.com>
2021-05-17 03:44:09 +0200

    fix: bandaid fix for the blogs pages

--- pages/blogs/_id.vue
+++ pages/blogs/_id.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
-
     <div class="theme-hero-area theme-hero-area-half">
       <div class="theme-hero-area-bg-wrap">
-        <div class="theme-hero-area-bg" style="background-image:url('https://www.happyvoyaging.com/themes/mmpt21/assets/img/new-york-city-city-street-cars-501334_1500x800.jpeg');"></div>
+        <div
+          class="theme-hero-area-bg"
+          style="
+            background-image: url('https://www.happyvoyaging.com/themes/mmpt21/assets/img/new-york-city-city-street-cars-501334_1500x800.jpeg');
+          "
+        ></div>
         <div class="theme-hero-area-mask theme-hero-area-mask-half"></div>
         <div class="theme-hero-area-inner-shadow"></div>
       </div>
@@ -13,7 +17,7 @@
           <div class="row">
             <div class="col-md-8 col-md-offset-2 theme-page-header-abs">
               <div class="theme-page-header theme-page-header-lg">
-                <h1 class="theme-page-header-title">{{blogs.response.title}}</h1>
+                <h1 class="theme-page-header-title">{{ blog.title }}</h1>
                 <p class="theme-page-header-subtitle">posted 5 days ago</p>
               </div>
             </div>
@@ -27,15 +31,13 @@
         <div class="row">
           <div class="col-md-8 col-md-offset-2">
             <div class="theme-blog-post">
-
-                <div v-html="blogs.response.description" class="blogdesc"></div>
-              
+              <!-- eslint-disable-next-line vue/no-v-html -->
+              <div class="blogdesc" v-html="blog.description"></div>
             </div>
           </div>
         </div>
       </div>
     </div>
-
 
     <div class="theme-page-section theme-page-section-xl">
       <div class="container">
@@ -43,41 +45,35 @@
           <h5 class="theme-page-section-title">Further Reading</h5>
           <p class="theme-page-section-subtitle">In etiam magnis scelerisque blandit</p>
         </div>
-        <div class="theme-inline-slider row" data-gutter="10">
-            <no-ssr> 
-
-            </no-ssr>     
-        </div>
+        <div class="theme-inline-slider row" data-gutter="10"></div>
       </div>
     </div>
-
-  </div>    
+  </div>
 </template>
 
 <script>
 export default {
-   ssr: false,
-    data(){
-            return {
-                blogs: []
-            }
-        },
-        async fetch() {
-            const blogslug = this.$route.params.id
-            const blogid = blogslug.split("-")[0]
-            const response = await fetch('https://www.happyvoyaging.com/api/blog/detail?id='+blogid+'')
-            this.blogs = await response.json()
-        }
+  // ssr: false,
 
+  data() {
+    return {
+      blog: [],
+    }
+  },
+  async fetch() {
+    const blogslug = this.$route.params.id
+    const blogid = blogslug.split('-')[0]
+    const response = await fetch(`https://www.happyvoyaging.com/api/blog/detail?id=${blogid}`)
+    const jsonedResponse = await response.json()
+    this.blog = jsonedResponse.response
+  },
 }
 </script>
 
-<style>
-
+<style scoped>
 .blogdesc img {
-    vertical-align: middle;
-    min-width: -webkit-fill-available!important;
-    max-width: -webkit-fill-available!important;
+  vertical-align: middle;
+  min-width: -webkit-fill-available !important;
+  max-width: -webkit-fill-available !important;
 }
-
 </style>
--- pages/blogs/index.vue
+++ pages/blogs/index.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
-
     <div class="theme-hero-area theme-hero-area-half">
       <div class="theme-hero-area-bg-wrap">
-        <div class="theme-hero-area-bg" style="background-image:url('https://www.happyvoyaging.com/themes/mmpt21/assets/img/activity-adult-beach-beautiful-378152_1500x800.jpeg');"></div>
+        <div
+          class="theme-hero-area-bg"
+          style="
+            background-image: url('https://www.happyvoyaging.com/themes/mmpt21/assets/img/activity-adult-beach-beautiful-378152_1500x800.jpeg');
+          "
+        ></div>
         <div class="theme-hero-area-mask theme-hero-area-mask-half"></div>
         <div class="theme-hero-area-inner-shadow"></div>
       </div>
@@ -19,87 +23,76 @@
     <div class="theme-page-section theme-page-section-xl theme-page-section-gray">
       <div class="container">
         <div class="row row-col-gap" data-gutter="20">
-
           <div v-for="blog in posts" :key="blog.id" class="col-md-3">
-                   <div class="theme-blog-item _br-4 theme-blog-item-full">
-                       <a :href="`/blogs/${blog.id}-${blog.slug}`" class="theme-blog-item-link"></a>
-                      <!-- <nuxt-link :to="`/blogs/${blog.id}-${blog.slug}`" class="theme-blog-item-link"></nuxt-link> -->
-                       <div class="banner _h-45vh banner- banner-animate banner-animate-zoom-in banner-animate-slow">
-                           <div class="banner-bg" v-bind:style="`background-image:url('${blog.thumbnail}');`"></div> 
-                           <div class="banner-caption banner-caption-bottom banner-caption-grad">
-                               <p class="theme-blog-item-time">{{ blog.day }}</p> 
-                               <h5 class="theme-blog-item-title">{{ blog.title }}</h5>
-                                <p class="theme-blog-item-desc">{{ blog.description.substring(0,65)+"...." }}</p>
-                            </div>
-                        </div>
-                    </div>
+            <div class="theme-blog-item _br-4 theme-blog-item-full">
+              <nuxt-link
+                :to="{ path: `/${$i18n.locale}/blogs/${blog.id}` }"
+                class="theme-blog-item-link"
+              ></nuxt-link>
+              <!-- <nuxt-link :to="`/blogs/${blog.id}-${blog.slug}`" class="theme-blog-item-link"></nuxt-link> -->
+              <div class="banner _h-45vh banner- banner-animate banner-animate-zoom-in banner-animate-slow">
+                <div class="banner-bg" :style="`background-image:url('${blog.thumbnail}');`"></div>
+                <div class="banner-caption banner-caption-bottom banner-caption-grad">
+                  <p class="theme-blog-item-time">{{ blog.day }}</p>
+                  <h5 class="theme-blog-item-title">{{ blog.title }}</h5>
+                  <p class="theme-blog-item-desc">{{ blog.description.substring(0, 65) + '....' }}</p>
+                </div>
+              </div>
             </div>
-
+          </div>
         </div>
-         <infinite-loading
-            v-if="posts.length" 
-              spinner="spiral"
-              @infinite="infiniteScroll"
-            >
-            
-            </infinite-loading>
+        <infinite-loading v-if="posts.length" spinner="spiral" @infinite="infiniteScroll"></infinite-loading>
       </div>
-    </div>   
-
-  </div>   
+    </div>
+  </div>
 </template>
 
 <script>
-	import axios from 'axios'
-	import InfiniteLoading from 'vue-infinite-loading'
-	export default {
-		name: 'Posts',		
-		data() {
-			return {
-				posts: [],
-				page: 1				
-			}
-		},
-		computed: {
-			url() {				
-				return 'https://www.happyvoyaging.com/api/blog/list'
-			}
-		},
-		created() {
-			this.fetchData()
-		},  
-		methods: {
-			async fetchData() {
-				const res = await axios.get(this.url)
-				this.posts = res.data.response.posts	 
-			},
-			infiniteScroll($state) {				
-				setTimeout(() => {
-					this.page++					
-					axios
-					.get(this.url)
-					.then((res) => {						
-						if (res.data.response.posts.length > 1) {
-							res.data.response.posts.forEach((item) => this.posts.push(item))
-              //console.log(response)
-							$state.loaded()							
-							} else {							
-							$state.complete()
-							}
-					})
-					.catch((err) => {
-						console.log(err)
-					})
-				}, 1000)
-			}
-		},
-		components: {
-			InfiniteLoading
-		}
-    
-	}
+import axios from 'axios'
+import InfiniteLoading from 'vue-infinite-loading'
+export default {
+  name: 'Posts',
+  components: {
+    InfiniteLoading,
+  },
+  data() {
+    return {
+      posts: [],
+      page: 1,
+    }
+  },
+  computed: {
+    url() {
+      return 'https://www.happyvoyaging.com/api/blog/list'
+    },
+  },
+  async created() {
+    await this.fetchData()
+  },
+  methods: {
+    async fetchData() {
+      const res = await axios.get(this.url)
+      this.posts = res.data.response.posts
+    },
+    infiniteScroll($state) {
+      setTimeout(() => {
+        this.page++
+        axios
+          .get(this.url)
+          .then((res) => {
+            if (res.data.response.posts.length > 1) {
+              res.data.response.posts.forEach((item) => this.posts.push(item))
+              // console.log(response)
+              $state.loaded()
+            } else {
+              $state.complete()
+            }
+          })
+          .catch((err) => {
+            console.log(err)
+          })
+      }, 1000)
+    },
+  },
+}
 </script>
-
-<style>
-
-</style>
